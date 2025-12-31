a319e0f7148ae1cb7eb597994ba599b70731cfff
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-12-31 12:37:25 +0200

    lakanto

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -158,88 +158,86 @@
 
 
 
-    <!--== Start Product Area Wrapper ==-->
-    <section class="product-area">
-        <div class="container pt-95 pt-lg-70">
-          <div class="row">
+<!--== Start Product Area Wrapper ==-->
+<section class="product-area">
+    <div class="container pt-95 pt-lg-70">
+        <div class="row">
             <div class="col-sm-8 m-auto">
-              <div class="section-title text-center">
-                <h2 class="title">Featured Products</h2>
-                <div class="desc">
-                  <p>Add our Featured Products to your weekly lineup</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <div class="product-slider owl-carousel owl-theme">
-                @if($featuredProducts)
-                @foreach ($featuredProducts as $productItem)
-                <div class="item">
-                  <!--== Start Shop Item ==-->
-                  <div class="product-item">
-                    <div class="inner-content">
-                      <div class="product-thumb">
-                        @if($productItem->productImages->count() > 0)    
-                        <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
-                            <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
-                            @if($productItem->productImages->count() > 1)
-                                <img class="second-image" src="{{ asset($productItem->productImages[1]->image) }}" alt="{{ $productItem->name }}">
-                            @endif
-                        </a>
-                    @endif
-                    <livewire:frontend.indexwish :product="$product"/>
-    <div class="white-bg">                           
-                               <livewire:frontend.cart.add-to-cart :product="$product"/>
-                               </div>
-                        <ul class="product-flag">
-                          <li class="new">
-                            @if ($productItem->quantity > 0)
-                            <span>In Stock</span>
-                        @else
-                          Out of Stock
-                        @endif</li>
-                        @if($productItem->original_price)
-                        @php
-                          $discount = (($productItem->original_price - $productItem->selling_price) / $productItem->original_price ) * 100;
-                        @endphp
-                      
-                        <li class="discount">-{{ round($discount , 0) }}%</li>
-                      @endif
-                        </ul>
-                      </div>
-                      <div class="product-desc">
-                        <div class="product-info">
-                          <h4 class="title"><a href="single-product.html">{{ $productItem->small_description }}</a></h4>
-                 
-                        <div class="prices">
-                            @if($productItem->original_price)
-                                <span class="price-old">${{ $productItem->original_price }}</span>
-                            @endif
-                            <span class="price">${{ $productItem->selling_price }}</span>
-                        </div>
-                        @if ($productItem->quantity > 0)
-                            <span class="product__badge product__badge_success">In Stock</span>
-                        @else
-                            <span class="product__badge">Out of Stock</span>
-                        @endif
-                        </div>
-     
-                 
-                      </div>
+                <div class="section-title text-center">
+                    <h2 class="title">Featured Products</h2>
+                    <div class="desc">
+                        <p>Add our Featured Products to your weekly lineup</p>
                     </div>
-                  </div>
-                  <!--== End Shop Item ==-->
                 </div>
-                @endforeach
-                @endif
-              </div>
             </div>
-          </div>
         </div>
-      </section>
-      <!--== End Product Area Wrapper ==-->
+        <div class="row">
+            <div class="col-12">
+                <div class="product-slider owl-carousel owl-theme">
+                    @if($featuredProducts)
+                        @foreach ($featuredProducts as $productItem)
+                            <div class="item">
+                                <!--== Start Shop Item ==-->
+                                <div class="product-item">
+                                    <div class="inner-content">
+                                        <div class="product-thumb">
+                                            @if($productItem->productImages->count() > 0)    
+                                                <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
+                                                    <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
+                                                    @if($productItem->productImages->count() > 1)
+                                                        <img class="second-image" src="{{ asset($productItem->productImages[1]->image) }}" alt="{{ $productItem->name }}">
+                                                    @endif
+                                                </a>
+                                            @endif
+                                            <!-- Fix here: change $product to $productItem -->
+                                            <livewire:frontend.indexwish :product="$productItem"/>
+                                            <div class="white-bg">                           
+                                                <livewire:frontend.cart.add-to-cart :product="$productItem"/>
+                                            </div>
+                                            <ul class="product-flag">
+                                                <li class="new">
+                                                    @if ($productItem->quantity > 0)
+                                                        <span>In Stock</span>
+                                                    @else
+                                                        Out of Stock
+                                                    @endif
+                                                </li>
+                                                @if($productItem->original_price)
+                                                    @php
+                                                        $discount = (($productItem->original_price - $productItem->selling_price) / $productItem->original_price ) * 100;
+                                                    @endphp
+                                                    <li class="discount">-{{ round($discount , 0) }}%</li>
+                                                @endif
+                                            </ul>
+                                        </div>
+                                        <div class="product-desc">
+                                            <div class="product-info">
+                                                <h4 class="title"><a href="single-product.html">{{ $productItem->small_description }}</a></h4>
+                                                <div class="prices">
+                                                    @if($productItem->original_price)
+                                                        <span class="price-old">${{ $productItem->original_price }}</span>
+                                                    @endif
+                                                    <span class="price">${{ $productItem->selling_price }}</span>
+                                                </div>
+                                                @if ($productItem->quantity > 0)
+                                                    <span class="product__badge product__badge_success">In Stock</span>
+                                                @else
+                                                    <span class="product__badge">Out of Stock</span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--== End Shop Item ==-->
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--== End Product Area Wrapper ==-->
 
 
  <!--== Start Testimonial Area Wrapper ==-->
